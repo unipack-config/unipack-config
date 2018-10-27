8b58bed52ba324d4a7c42c02a5ceb9e5d9000373
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-27 10:57:03 -0400

    NEW DESIGN COMPLETE

--- src/client/Console.js
+++ src/client/Console.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 const wrap = {
-  width: "20%",
-  borderRight: "2px solid #FF5D52"
+  width: "435px",
+  height: '95vh',
+  padding: '0px 25px 125px 10px',
+  overflow: "scroll",
 };
 
 const fieldWrap = {
@@ -14,117 +16,110 @@ export const Console = props => {
     <div style={wrap}>
       <div style={fieldWrap}>
         {/* MODE */}
-        <label>Mode</label>
-        <select onChange={(e) => {props.modeHandler(e)}}>
-          <option value="none">None</option>
-          <option value="development">Development</option>
-          <option value="production">Production</option>
+        <label class="form-label">Mode</label>
+        <select class="custom-select" onChange={(e) => {props.modeHandler(e)}}>
+          <option class="select-items" value="none">None</option>
+          <option class="select-items" value="development">Development</option>
+          <option class="select-items" value="production">Production</option>
         </select>
         </div>
 
         <div style={fieldWrap}>
         {/* ENTRY */}
-        <label>Entry point</label>
-        <input value={props.entry} onChange={(e) => {props.entryHandler(e)}} placeholder="Please enter entry point"/>
+        <label class="form-label">Entry point</label>
+        <input class="input-form" value={props.entry} onChange={(e) => {props.entryHandler(e)}} placeholder="Please enter entry point"/>
         </div>
 
         {/* OUTPUT */}
         <div style={fieldWrap}>
-          <label>Output Path</label>
-          <input value={props.path} onChange={(e) => props.outputPathHandler(e)} type="text" />
+          <label class="form-label">Output Path</label>
+          <input class="input-form" value={props.path} onChange={(e) => props.outputPathHandler(e)} type="text" />
         </div>
 
         <div style={fieldWrap}>
-          <label>Output Filename</label>
-          <input value={props.filename} onChange={(e) => props.outputFilenameHandler(e)} type="text" />
+          <label class="form-label">Output Filename</label>
+          <input class="input-form" value={props.filename} onChange={(e) => props.outputFilenameHandler(e)} type="text" />
         </div>
 
         {/* MODULE */}
         <div style={fieldWrap}>
-          <label>Module</label>
-          <input onClick={props.moduleHandler} type="button" />
-          <input type="text" placeholder="Please enter your module" />
+          <label class="form-label">Module</label>
+          <input class="input-form" type="text" placeholder="Please enter your module" />
         </div>
 
         {/* RESOLVE */}
         <div style={fieldWrap}>
-          <label>Resolve</label>
-          <input onClick={props.resolveHandler} type="button" />
-          <input type="text" placeholder="Please enter your module" />
+          <label class="form-label">Resolve</label>
+          <input class="input-form" type="text" placeholder="Please enter your module" />
         </div>
 
         {/* PERFORMANCE */}
         <div style={fieldWrap}>
-          <label>Performance</label>
-          <input onClick={props.performanceHandler} type="button" />
-          <input type="text" placeholder="Please enter your module" />
+          <label class="form-label">Performance</label>
+          <input class="input-form" type="text" placeholder="Please enter your module" />
         </div>
 
         <div style={fieldWrap}>
           {/* DEVTOOL */}
-          <label>Devtool</label>
-          <select onChange={props.devtoolHandler}>
-            <option value="source-map" >source-map</option>
-            <option value="eval" >eval</option>
-            <option value="cheap-eval-source-map">cheap-eval-source-map</option>
-            <option value="eval-source-map">eval-source-map</option>
-            <option value="cheap-source-map">cheap-source-map</option>
-            <option value="cheap-module-source-map">cheap-module-source-map</option>
-            <option value="inline-cheap-source-map">inline-cheap-source-map</option>
-            <option value="inline-cheap-module-source-map">inline-cheap-module-source-map</option>
-            <option value="hidden-source-map">hidden-source-map</option>
-            <option value="inline-source-map">inline-source-map</option>
-            <option value="nosources-source-map">nosources-source-map</option>
+          <label class="form-label">Devtool</label>
+          <select class="custom-select" onChange={props.devtoolHandler}>
+            <option class="select-items" value="source-map" >source-map</option>
+            <option class="select-items" value="eval" >eval</option>
+            <option class="select-items" value="cheap-eval-source-map">cheap-eval-source-map</option>
+            <option class="select-items" value="eval-source-map">eval-source-map</option>
+            <option class="select-items" value="cheap-source-map">cheap-source-map</option>
+            <option class="select-items" value="cheap-module-source-map">cheap-module-source-map</option>
+            <option class="select-items" value="inline-cheap-source-map">inline-cheap-source-map</option>
+            <option class="select-items" value="inline-cheap-module-source-map">inline-cheap-module-source-map</option>
+            <option class="select-items" value="hidden-source-map">hidden-source-map</option>
+            <option class="select-items" value="inline-source-map">inline-source-map</option>
+            <option class="select-items" value="nosources-source-map">nosources-source-map</option>
           </select>
         </div>
 
       <div style={fieldWrap}>
           {/* Context */}
-          <label>Context</label>
-          <input onClick={props.contextHandler} type="button" />
-          <input type="text" placeholder="Please enter your module" />
+          <label class="form-label">Context</label>
+          <input class="input-form" type="text" placeholder="Please enter your module" />
         </div>
 
       <div style={fieldWrap}>
           {/* Target */}
-          <label>Target</label>
-          <input onClick={props.targetHandler} type="button" />
-          <input type="text" placeholder="Please enter your module" />
+          <label class="form-label">Target</label>
+          <input class="input-form" type="text" placeholder="Please enter your module" />
         </div>
 
       <div style={fieldWrap}>
           {/* Externals */}
-          <label>Externals</label>
-          <input onClick={props.externalsHandler} type="button" />
-          <input type="text" placeholder="Please enter your module" />
+          <label class="form-label">Externals</label>
+          <input class="input-form" type="text" placeholder="Please enter your module" />
         </div>
 
       <div style={fieldWrap}>
           {/* STATS */}
-          <label>Stats</label>
-          <select onChange={props.statsHandler} >
-            <option>none</option>
-            <option>errors-only</option>
-            <option>minimal</option>
-            <option>normal</option>
-            <option>verbose</option>
+          <label class="form-label">Stats</label>
+          <select class="custom-select" onChange={props.statsHandler} >
+            <option class="select-items">none</option>
+            <option class="select-items">errors-only</option>
+            <option class="select-items">minimal</option>
+            <option class="select-items">normal</option>
+            <option class="select-items">verbose</option>
           </select>
   
         </div>
 
       <div style={fieldWrap}>
           {/* DEVSERVER */}
-          <label>DevServer</label>
-          <select onChange={props.devserverHandler}>
+          <label class="form-label">DevServer</label>
+          <select class="custom-select" onChange={props.devserverHandler}>
             <option>proxy</option>
           </select>
         </div>
 
       <div style={fieldWrap}>
           {/* PLUGINS */}
-          <label>Plugins</label>
-          <input onClick={props.pluginsHandler} type="button" />
-          <input type="text" placeholder="Please enter your module" />
+          <label class="form-label">Plugins</label>
+          <input class="input-form" type="text" placeholder="Please enter your module" />
         </div>
       
     </div>
